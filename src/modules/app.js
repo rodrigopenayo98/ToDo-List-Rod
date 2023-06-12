f12bfdc274fd37b79ab4c743c54c32475e63ad1d
rodrigopenayo98 <91923422+rodrigopenayo98@users.noreply.github.com>
2023-06-11 22:35:45 -0300

    fix linters

--- src/modules/app.js
+++ src/modules/app.js
@@ -70,6 +70,7 @@ const generateTaskList = () => {
 };
 
 const addTask = (description, completed, index) => {
+  // eslint-disable-next-line new-cap
   const taskAdded = new StructureTask(description, completed, index);
   localGet.push(taskAdded);
   localStorage.setItem('listInMemory', JSON.stringify(localGet));
