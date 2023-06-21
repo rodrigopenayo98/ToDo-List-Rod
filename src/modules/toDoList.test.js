0238d7025c96a41dc6bc5cf2e2d8cb0a6ccc9ac6
Sive Mdluli <mdlulisive202@gmail.com>
2023-06-21 19:10:38 +0200

    Add tests for the removeTask functionality

--- src/modules/toDoList.test.js
+++ src/modules/toDoList.test.js
@@ -1,5 +1,5 @@
-const { describe, test } = require('@jest/globals');
-const { addTask } = require('./app.js');
+const { describe, test } = require("@jest/globals");
+const { addTask } = require("./app.js");
 
 const localStorageMock = (() => {
   let store = {};
@@ -26,21 +26,60 @@ document.body.innerHTML = `
   </ul>
 `;
 
-Object.defineProperty(window, 'localStorage', { value: localStorageMock });
+Object.defineProperty(window, "localStorage", { value: localStorageMock });
 
-describe('addTask', () => {
-  test('Adds a task to the list and updates localStorage', () => {
+describe("addTask", () => {
+  test("Adds a task to the list and updates localStorage", () => {
     localStorage.clear();
-    addTask('Descripción de la tarea', false, 0);
+    addTask("Descripción de la tarea", false, 0);
 
-    const taskList = document.getElementById('taskList');
+    const taskList = document.getElementById("taskList");
 
     expect(taskList.children.length).toBe(1);
 
-    expect(localStorage.getItem('listInMemory')).toEqual(JSON.stringify([{
-      description: 'Descripción de la tarea',
-      completed: false,
-      index: 0,
-    }]));
+    expect(localStorage.getItem("listInMemory")).toEqual(
+      JSON.stringify([
+        {
+          description: "Descripción de la tarea",
+          completed: false,
+          index: 0,
+        },
+      ])
+    );
+  });
+});
+
+describe("removeTask", () => {
+  test("Removes a task from the list and updates localStorage", () => {
+    localStorage.clear();
+    addTask("Description of task 1", false, 0);
+    addTask("Description of task 2", false, 1);
+    addTask("Description of task 3", false, 2);
+
+    const taskList = document.getElementById("taskList");
+    const trashElement = taskList.querySelector(".trash");
+    const parentElement = trashElement.parentNode;
+
+    parentElement.dispatchEvent(new Event("click"));
+
+    setTimeout(() => {
+      const updatedTaskList = document.getElementById("taskList");
+      const removedElement = updatedTaskList.querySelector(".item-task");
+      expect(removedElement).toBeNull();
+      expect(localStorage.getItem("listInMemory")).toEqual(
+        JSON.stringify([
+          {
+            description: "Description of task 1",
+            completed: false,
+            index: 0,
+          },
+          {
+            description: "Description of task 3",
+            completed: false,
+            index: 1,
+          },
+        ])
+      );
+    }, 0);
   });
 });
